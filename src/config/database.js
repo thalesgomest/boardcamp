2237c1b98ac160f7477e37fe88762d75f421d964
Thales Gomes Targino <thalestargino@gmail.com>
2022-05-30 00:13:18 -0300

    fix: deploy heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,14 @@
 import pg from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const { Pool } = pg;
 
 const databaseConfig = {
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 };
 
-if (process.env.MODE === 'PROD') {
-    databaseConfig.ssl = {
-        rejectUnauthorized: false,
-    };
-}
-const connection = new Pool({ databaseConfig });
+const connection = new Pool(databaseConfig);
 
 export default connection;
